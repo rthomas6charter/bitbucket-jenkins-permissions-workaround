52bf12d5ee7e314f690ecbd196c8d4b86ff0285a
Thomas, Ray <Ray.Thomas@charter.com>
2016-09-07 17:54:26 -0600

    Got everything to work once.

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# bitbucket-jenkins-permissions-workaround
-Shell script to allow a build job in Jenkins (or Bamboo or similar build tool) to work around bitbucket's "No changes without a pull request" branch restriction.
+# BitBucket Build System Permissions Workaround
 
-* Note: This depends on having 2 BitBucket users defined for use by the build system.  For instance, JenkinsUser and JenkinsApprovalUser.
+This is a workaround for the issue described 
+      here: https://jira.atlassian.com/browse/BSERV-8647
+  and here: https://jira.atlassian.com/browse/BSERV-7475
+
+* Note that there is an option added in BitBucket 4.5.1 that allows a user (e.g. JenkinsUser or BambooUser) to be exempted from the branch restriction that enforces "No changes without a pull request".  Upgrading to 4.5.1 or later would be preferred to this workaround.
+
+* The shell script allow a build job in Jenkins (or Bamboo or similar build tool) to work around bitbucket's "No changes without a pull request" branch restriction and "Requires (x) reviewers" pull-request restriction by automating the process of creating a branch and a pull-request with reviewers, and then approving and merging the pull request.
+
+* Note: To comply with both the "No changes without a pull request" and the "Requires (x) reviewers" restrictions, this depends on having 2 BitBucket users defined for use by the build system.  For instance, JenkinsUser to checkout, branch, commit, push, and merge, and JenkinsApprovalUser to serve as a separate "approver".  If more than one reviewer is required, this would need to be modified to add multiple reviewers and submit approvals from all of them.
 
 * Note: The sample .bitbucket_rest_properties file would be moved into the profile/user directory in the OS for
 the user that runs the Jenkins worker-process/job (e.g. /home/JenkinsWorkerAccount/.bitbucket_rest_properties).
--- commit_changes_via_pull_request.sh
+++ commit_changes_via_pull_request.sh
@@ -7,10 +7,10 @@
 #
 # Note that there is an option added in BitBucket 4.5.1 that
 # allows a user (e.g. JenkinsUser or BambooUser) to be exempted from the 
-# restriction prohibiting "Changes without a pull request"
+# restriction enforcing "No changes without a pull request"
 # which would be preferred to this workaround.
 #
-# REF: https://developer.atlassian.com/static/rest/bitbucket-server/4.9.0/bitbucket-rest.html
+# REF: https://developer.atlassian.com/static/rest/bitbucket-server/4.3.1/bitbucket-rest.html
 # REF: https://developer.atlassian.com/bitbucket/server/docs/latest/how-tos/command-line-rest.html
 ####################################################################################################
 
@@ -18,6 +18,37 @@
 # doing something besides updating the Maven artifact version.
 # It can be changed to whatever makes sense.
 base_branch_name="jenkins_pom_vers_update_"
+base_pull_request_title="Jenkins Build Automation "
+
+# Expected properties are.
+# base_rest_url - the bitbucket server's rest API url path
+#       example: http://bitbucket.example.com/bitbucket/rest/api/1.0
+# starting_branch - the branch from which the temporary modification branch should be created
+#       example: develop
+# primary_username - the main bitbucket user account name (permitted to create branches, merge/write, etc.)
+#       example: JenkinsUser
+# primary_password - the password to use when authenticating the user specified by primary_username
+# secondary_username - the bitbucket user account name used for approving a pull request
+#       example: JenkinsApprovalUser
+# secondary_password - the password to use when authenticating the user specified by secondary_username
+#
+# Note: Because this file contains passwords, it should be kept in a user directory that
+# is only accessible by the OS (e.g. Linux) user account that runs the build process.
+# Also, permissions should be set to 600 (i.e. user=rw, group=none, others=none)
+load_properties() {
+	local bitbucket_rest_properties_file=~/.bitbucket_rest_properties
+
+	if [ -f "$bitbucket_rest_properties_file" ]
+	then
+	    # echo "Found ${bitbucket_rest_properties_file}."
+	    while IFS='=' read -r key value
+	    do
+	    	eval "${key}='${value}'"
+	    done < "$bitbucket_rest_properties_file"
+	else
+		echo "Missing required config file: ${bitbucket_rest_properties_file}"
+	fi
+}
 
 usage() {
 	# Note: Calling the third parameter new_version in the usage help
@@ -32,26 +63,32 @@ usage() {
 }
 
 # Verify that the inputs are present and, as much as possible, that they look right
-check_inputs() {
+# TODO: get the project and repo names from the context git repo/clone instead
+# of requiring them to be passed as inputs.
+check_inputs_and_context() {
 	if [ $# -lt 3 ]
 	then
 		echo "Missing required arguments."
 		usage
 	fi
-}
-
-load_properties() {
-	bitbucket_rest_properties_file=~/.bitbucket_rest_properties
-
-	if [ -f "$bitbucket_rest_properties_file" ]
+	local scriptname=$(basename "$0")
+	if [ -f "./${scriptname}" ]
 	then
-	    echo "Found ${bitbucket_rest_properties_file}."
-	    while IFS='=' read -r key value
-	    do
-	    	eval "${key}='${value}'"
-	    done < "$bitbucket_rest_properties_file"
-	else
-		echo "Missing required config file: ${bitbucket_rest_properties_file}"
+	    echo "Do not run this from the script directory where ${scriptname} is located."
+	    echo "It must be run from the base directory of the local git repository (clone) of the BitBucket repository that is being modified."
+	    exit 1
+	fi
+	local currentbranch=$(git branch | grep "\*" | cut -d ' ' -f2)
+	if [ -z ${starting_branch+x} ]
+	then
+	    echo "Missing property value for \$starting_branch"
+	    exit 1
+	fi
+	if [ $currentbranch != $starting_branch ]
+	then
+	    echo "The local git repository (clone) must be switched to branch: '${starting_branch}' before running this script."
+	    echo "It is currently set to branch: '${currentbranch}'."
+	    exit 1
 	fi
 }
 
@@ -65,16 +102,23 @@ create_modified_branch_in_remote_bitbucket() {
 modify_source_files() {
 	local new_version=$1
     #
-	#  ADD ANY OTHER AUTOMATED SOURCE CHANGES HERE!!!
+	#  CHANGE THIS TO PERFORM WHATEVER BUILD AUTOMATED SOURCE FILE MODIFICATON
+	#  IS DESIRED.
 	#
 	# As an example of a typical build automated change, this script just 
 	# changes the version in the pom.xml file(s)
 	mvn -DnewVersion=${new_version} -DgenerateBackupPoms=false versions:set
+	
+	# Other suggestions...
+    # * Run a linter/formatter on source code to correct non-compliant formatting.
+    # * Insert Copyright/timestamp/release-version-markers/etc in source files.
+
 }	
 
 commit_and_push_modified_source() {
 	local branch_name_suffix=$1
 	# commit locally
+	git add .
 	git commit -m "Build Job modifications to branch with pull request restriction."
 	# push to bitbucket remote (creating the tracking branch at the same time)
 	git push --set-upstream origin "${base_branch_name}${branch_name_suffix}"
@@ -83,15 +127,15 @@ commit_and_push_modified_source() {
 build_pull_request_json() {
 	local bitbucket_project=$1
 	local bitbucket_repo=$2
-	local pull_request_name=$3
+	local branch_name_suffix=$3
 	local pull_request_json="{"
-	pull_request_json="${pull_request_json} \"title\": \"Jenkins Release Version pom.xml Update\""
-	pull_request_json="${pull_request_json} ,\"description\": \"Jenkins release build automation.\""
+	pull_request_json="${pull_request_json} \"title\": \"${base_pull_request_title}${branch_name_suffix}\""
+	pull_request_json="${pull_request_json} ,\"description\": \"Build automation.\""
 	pull_request_json="${pull_request_json} ,\"state\": \"OPEN\""
 	pull_request_json="${pull_request_json} ,\"open\": \"true\""
 	pull_request_json="${pull_request_json} ,\"closed\": \"false\""
 	pull_request_json="${pull_request_json} ,\"fromRef\": {"
-	pull_request_json="${pull_request_json}     \"id\": \"refs/heads/${base_branch_name}_${pull_request_name}\""
+	pull_request_json="${pull_request_json}     \"id\": \"refs/heads/${base_branch_name}${branch_name_suffix}\""
 	pull_request_json="${pull_request_json}     ,\"repository\": {"
 	pull_request_json="${pull_request_json}         \"slug\": \"${bitbucket_repo}\""
 	pull_request_json="${pull_request_json}         ,\"name\": null"
@@ -101,7 +145,7 @@ build_pull_request_json() {
 	pull_request_json="${pull_request_json}     }"
 	pull_request_json="${pull_request_json} }" # close fromRef
 	pull_request_json="${pull_request_json} ,\"toRef\": {"
-	pull_request_json="${pull_request_json}     \"id\": \"refs/heads/develop\""
+	pull_request_json="${pull_request_json}     \"id\": \"refs/heads/${starting_branch}\""
 	pull_request_json="${pull_request_json}     ,\"repository\": {"
 	pull_request_json="${pull_request_json}         \"slug\": \"${bitbucket_repo}\""
 	pull_request_json="${pull_request_json}         ,\"name\": null"
@@ -130,23 +174,34 @@ build_pull_request_json() {
 }
 
 create_pull_request() {
-	local bitbucket_project=$2
-	local bitbucket_repo=$3
-	local pull_request_name=$1
-	local create_pull_request_body=$(build_pull_request_json $pull_request_name $bitbucket_project $bitbucket_repo)
-    local create_pull_request_curl_cmd="curl --request POST --url ${base_rest_url}/projects/${bitbucket_project}/repos/${bitbucket_repo}/pull-requests"
-    create_pull_request_curl_cmd="${create_pull_request_curl_cmd} --user '${primary_username}:${primary_password}'"
-    create_pull_request_curl_cmd="${create_pull_request_curl_cmd} --header 'cache-control: no-cache'"
-    create_pull_request_curl_cmd="${create_pull_request_curl_cmd} --header 'content-type: application/json'"
-    create_pull_request_curl_cmd="${create_pull_request_curl_cmd} --data ${create_pull_request_body}"
-    echo "TODO: actually execute: $create_pull_request_curl_cmd"
-	# Note: This will sort the json alphabetically for display
-	# Use yajl json_reformat instead if it becomes important to see it in the original order.
-	# See: http://www.skorks.com/2013/04/the-best-way-to-pretty-print-json-on-the-command-line/
-	echo "TODO: actually send body:" 
-    echo "${create_pull_request_body}" | python -m json.tool
-    #TODO: parse response and capture the pull-request id (should be numeric)
-    local pull_request_id=1
+	local bitbucket_project=$1
+	local bitbucket_repo=$2
+	local branch_name_suffix=$3
+	local create_pull_request_body=$(build_pull_request_json $bitbucket_project $bitbucket_repo $branch_name_suffix)
+   	# Note: Python's json.tool will sort the json alphabetically for display
+    # Use yajl json_reformat instead if it becomes important to see it in the original order.
+    # See: http://www.skorks.com/2013/04/the-best-way-to-pretty-print-json-on-the-command-line/
+    # echo "Sending:\n${create_pull_request_body}" | python -m json.tool
+    local create_pull_request_curl_args="-X POST --silent"
+    # Note: password may have special characters that would need to be escaped, so
+    # the -u,--user argument must be in single quotes to be interpreted as a literal string.
+    create_pull_request_curl_args="${create_pull_request_curl_args} -u '${primary_username}:${primary_password}'"
+    create_pull_request_curl_args="${create_pull_request_curl_args} -H \"cache-control: no-cache\""
+    create_pull_request_curl_args="${create_pull_request_curl_args} -H \"content-type: application/json\""
+    create_pull_request_curl_args="${create_pull_request_curl_args} --data '${create_pull_request_body}'"
+    create_pull_request_curl_args="${create_pull_request_curl_args} ${base_rest_url}/projects/${bitbucket_project}/repos/${bitbucket_repo}/pull-requests"
+    echo $create_pull_request_curl_args
+    local create_pull_request_response=$(eval "curl ${create_pull_request_curl_args}")
+    #echo "create pull request response"
+    # echo "$create_pull_request_response"
+
+    # TODO: check the response for errors and quit if there are any.
+
+    # parse response and capture the pull-request id (should be numeric)
+    # tr splits the response json into lines at every comma
+    # grep grabs the very first match on "id", which is the pull request id
+    # cut pulls out just the numeric value of the id
+    local pull_request_id=$(echo ${create_pull_request_response} | tr , '\n' | grep -m 1 "id" | cut -d ':' -f2)
     return ${pull_request_id}
 }
 
@@ -154,32 +209,54 @@ approve_pull_request() {
 	local bitbucket_project=$1
 	local bitbucket_repo=$2
 	local pull_request_id=$3
-    local approve_pull_request_curl_cmd="curl --request POST --url ${base_rest_url}/projects/${bitbucket_project}/repos/${bitbucket_repo}/pull-requests/${pull_request_id}"
-    approve_pull_request_curl_cmd="${approve_pull_request_curl_cmd}/participants/${secondary_username}"
-    approve_pull_request_curl_cmd="${approve_pull_request_curl_cmd} --user '${secondary_username}:${secondary_password}'"
-    approve_pull_request_curl_cmd="${approve_pull_request_curl_cmd} --header 'cache-control: no-cache'"
-    echo "TODO: actually execute: ${approve_pull_request_curl_cmd}"
+    local approve_pull_request_curl_args="-X PUT --silent"
+    approve_pull_request_curl_args="${approve_pull_request_curl_args} --user '${secondary_username}:${secondary_password}'"
+    approve_pull_request_curl_args="${approve_pull_request_curl_args} -H \"cache-control: no-cache\""
+    approve_pull_request_curl_args="${approve_pull_request_curl_args} -H \"content-type: application/json\""
+    approve_pull_request_curl_args="${approve_pull_request_curl_args} --data '{\"status\": \"APPROVED\"}'"
+    approve_pull_request_curl_args="${approve_pull_request_curl_args} ${base_rest_url}/projects/${bitbucket_project}/repos/${bitbucket_repo}/pull-requests/${pull_request_id}/participants/${secondary_username}"
+    echo "Calling: curl ${approve_pull_request_curl_args}"
+    local approve_pull_request_response=$(eval "curl ${approve_pull_request_curl_args}")
+
+    # TODO: check the response for errors and quit if there are any.
+
+    echo $approve_pull_request_response
 }
 
 merge_pull_request() {
 	local bitbucket_project=$1
 	local bitbucket_repo=$2
 	local pull_request_id=$3
-    local merge_pull_request_curl_cmd="curl --request GET --url ${base_rest_url}/projects/${bitbucket_project}/repos/${bitbucket_repo}/pull-requests/${pull_request_id}/merge"
-    merge_pull_request_curl_cmd="${merge_pull_request_curl_cmd} --user '${primary_username}:${primary_password}'"
-    merge_pull_request_curl_cmd="${merge_pull_request_curl_cmd} --header 'cache-control: no-cache'"
-    echo "TODO: actually execute: ${merge_pull_request_curl_cmd}"
+    local merge_pull_request_curl_args="-X POST --silent"
+    merge_pull_request_curl_args="${merge_pull_request_curl_args} --user '${primary_username}:${primary_password}'"
+    merge_pull_request_curl_args="${merge_pull_request_curl_args} -H \"cache-control: no-cache\""
+    merge_pull_request_curl_args="${merge_pull_request_curl_args} -H \"X-Atlassian-Token:nocheck\""
+    merge_pull_request_curl_args="${merge_pull_request_curl_args} ${base_rest_url}/projects/${bitbucket_project}/repos/${bitbucket_repo}/pull-requests/${pull_request_id}/merge?version=0"
+    echo "Calling: curl ${merge_pull_request_curl_args}"
+    local merge_pull_request_response=$(eval "curl ${merge_pull_request_curl_args}")
+
+    # TODO: check the response for errors and quit if there are any.
+
+    echo $merge_pull_request_response
+}
+
+cleanup_branch() {
+	local branch_name_suffix=$1
+	git checkout "${starting_branch}"
+	git branch -d "${base_branch_name}${branch_name_suffix}"
+	git push origin --delete "${base_branch_name}${branch_name_suffix}"
 }
 
 #########################################################################
 ##      Main script follows...                                         ##
 #########################################################################
-check_inputs "$@"
 load_properties
+check_inputs_and_context "$@"
 create_modified_branch_in_remote_bitbucket "$3"
 modify_source_files "$3"
 commit_and_push_modified_source "$3"
 create_pull_request "$1" "$2" "$3"
 pull_request_id=$?
-approve_pull_request "$1" "$2" $pull_request_id 
-merge_pull_request "$1" "$2" $pull_request_id
+approve_pull_request "$1" "$2" ${pull_request_id}
+merge_pull_request "$1" "$2" ${pull_request_id}
+cleanup_branch "$3"
